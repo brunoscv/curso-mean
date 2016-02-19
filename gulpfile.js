4322d0e7e72d5d29f2035745b470a68ff62ee40f
Rafaell Lycan <sonny.webdsg@gmail.com>
2016-02-19 01:48:24 -0200

    Add simple gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp       = require('gulp');
+var uglify     = require('gulp-uglify');
+var concat     = require('gulp-concat');
+var livereload = require('gulp-livereload');
+
+var source = {
+  npm: 'node_modules',
+  css: 'public/app/css',
+  js: 'public/app/js'
+};
+
+gulp.task('css', function () {
+  return livereload();
+});
+
+gulp.task('js', function() {
+  return gulp.src([
+    '!' + source.js + '/app.min.js',
+    source.npm + '/angular/angular.min.js',
+    source.npm + '/angular-ui-router/release/angular-ui-router.min.js',
+    source.npm + '/angular-resource/angular-resource.min.js',
+    source.js + '/app.js',
+    source.js + '/routes.js',
+    source.js + '/**/*.js'
+    ])
+    .pipe(concat('app.min.js'))
+    // .pipe(uglify({mangle: true}))
+    .pipe(gulp.dest(source.js))
+    .pipe(livereload());
+});
+
+gulp.task('default', ['css', 'js']);
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch(source.css + '/**/*.css', ['css']);
+  gulp.watch(['!' + source.js + '/app.min.js', source.js + '/**/*.js'], ['js']);
+});
