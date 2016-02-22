3356dae51aa42257bf7b8f2af735f1a54ebc27ed
Rafaell Lycan <sonny.webdsg@gmail.com>
2016-02-22 00:03:19 -0300

    Add some filters

--- public/app/js/controllers/HomeController.js
+++ public/app/js/controllers/HomeController.js
@@ -13,26 +13,64 @@
     vm.jobs = [];
     vm.page = 0;
     vm.size = 0;
-    vm.amount = 0;
+    vm.total = 0;
+
+    vm.filters = [];
+    vm.tags = [];
 
     vm.loadMore = loadMore;
+    vm.changeFilter = changeFilter;
+    vm.changeTag = changeTag;
 
     function loadMore (e) {
       e.preventDefault();
 
-      if (vm.amount > vm.jobs.length) {
+      if (vm.total > vm.jobs.length) {
         return loadJobs( (vm.page + 1) , SIZE);
       }
     }
 
     function loadJobs (page, size) {
-      JobService.get({ page: page || PAGE, size: size || SIZE}).$promise
+      JobService.get({
+        page: page || PAGE,
+        size: size || SIZE,
+        types: vm.filters.join(','),
+        tags: vm.tags.join(',')
+      }).$promise
         .then(function (response) {
           vm.jobs = vm.jobs.concat(response.items);
           vm.page = response._metadata.page;
           vm.size = response._metadata.size;
-          vm.amount = response._metadata.total;
+          vm.total = response._metadata.total;
         });
+    }
+
+    function changeFilter (jobType) {
+      var i = vm.filters.indexOf(jobType);
+
+      if (~i) {
+        vm.filters.splice(i, 1);
+      } else {
+        vm.filters.push(jobType);
+      }
+
+      loadJobs();
+      vm.jobs = [];
+    }
+
+    function changeTag (e, tag) {
+      e.preventDefault();
+
+      var i = vm.tags.indexOf(tag);
+
+      if (~i) {
+        vm.tags.splice(i, 1);
+      } else {
+        vm.tags.push(tag);
+      }
+
+      loadJobs();
+      vm.jobs = [];
     }
 
     loadJobs();
