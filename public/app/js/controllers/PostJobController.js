e4e199ed5dd792f5bbc0f0412f27ed5d7c048df4
Rafaell Lycan <sonny.webdsg@gmail.com>
2016-03-03 17:11:20 -0300

    Posting a job

--- public/app/js/controllers/PostJobController.js
+++ public/app/js/controllers/PostJobController.js
@@ -2,17 +2,29 @@
   angular.module('jobs')
     .controller('PostJobController', PostJobController);
 
-  PostJobController.$inject = ['CompanyService', '$state'];
+  PostJobController.$inject = ['JobService', '$state'];
 
-  function PostJobController (CompanyService, $state) {
+  function PostJobController (JobService, $state) {
     var vm = this;
 
-    vm.company = {
+    vm.jobTypes = [
+      'Full Time',
+      'Estágio',
+      'Freela'
+    ];
+
+    vm.job = {
       name : null,
+      location : null,
       description : null,
       remote : false,
+      type : null,
       tags : null,
     };
+
+    vm.submitJob = function () {
+      JobService.save(vm.job);
+    }
     
   }
 })();
